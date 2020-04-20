7fc73b591fbca35676140cc325be0fed0eee9a02
SpaceparticleJB <SpaceparticleJB@users.noreply.github.com>
2020-04-20 09:56:30 +0900

    SignUp ModelForms

--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 app_name = "users"
 
-
 urlpatterns = [
     path("login", views.LoginView.as_view(), name="login"),
     path("logout", views.log_out, name="logout"),
-    path("signup", views.SignUpView.as_view(), name="signup"),
+    path("sigup", views.SignUpView.as_view(), name="signup"),
 ]
