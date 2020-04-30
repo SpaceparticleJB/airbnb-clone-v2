824d112257711a42404bf437e3e4fc8ea77dcc90
SpaceparticleJB <SpaceparticleJB@users.noreply.github.com>
2020-04-30 23:50:26 +0900

    Update Profile

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("logout", views.log_out, name="logout"),
     path("signup", views.SignUpView.as_view(), name="signup"),
     path("verify/<str:key>", views.complete_verification, name="complete-verification"),
+    path("update-profile/", views.UpdateProfileView.as_view(), name="update"),
+    path("<int:pk>/", views.UserProfileView.as_view(), name="profile"),
     path("<int:pk>/", views.UserProfileView.as_view(), name="profile"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 import os
-from django.views.generic import FormView, DetailView
+from django.views.generic import FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from django.shortcuts import redirect, reverse
 from django.contrib.auth import authenticate, login, logout
@@ -62,3 +62,22 @@ def complete_verification(request, key):
 class UserProfileView(DetailView):
     model = models.User
     context_object_name = "user_obj"
+
+
+class UpdateProfileView(UpdateView):
+
+    model = models.User
+    template_name = "users/update-profile.html"
+    fields = (
+        "first_name",
+        "last_name",
+        "avatar",
+        "gender",
+        "bio",
+        "birthdate",
+        "language",
+        "currency",
+    )
+
+    def get_object(self, queryset=None):
+        return self.request.user
