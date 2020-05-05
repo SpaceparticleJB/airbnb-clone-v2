8bd0a76edac08b3dabca3e22c8c0b28e5eb3a3c3
SpaceparticleJB <SpaceparticleJB@users.noreply.github.com>
2020-05-05 21:03:28 +0900

    Adding Info to The Sessions

--- rooms/views.py
+++ rooms/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView, DetailView, View, UpdateView, FormVie
 from django.shortcuts import render, redirect, reverse
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from users import mixins as user_mixins
