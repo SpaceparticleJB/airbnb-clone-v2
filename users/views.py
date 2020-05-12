c0bd1f3bedf8a97f989dd744743665676240d71f
SpaceparticleJB <SpaceparticleJB@users.noreply.github.com>
2020-05-13 02:02:15 +0900

    Changing Language

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 import os
+from django.http import HttpResponse
 from django.contrib.auth.views import PasswordChangeView
+from django.utils import translation
 from django.views.generic import FormView, DetailView, UpdateView
 from django.urls import reverse_lazy
 from django.shortcuts import redirect, reverse
@@ -136,5 +138,5 @@ def switch_hosting(request):
 def switch_language(request):
     lang = request.GET.get("lang", None)
     if lang is not None:
-        pass
+        request.session[translation.LANGUAGE_SESSION_KEY] = lang
     return HttpResponse(status=200)
